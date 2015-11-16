2a77caa2deeb15a3ed6c3e796923eaef351e1614
TRUECLARITY\Nick Hills <nick.hills@trueclarity.co.uk>
2015-11-16 16:23:30 +0000

    Added attribute routing to the webapi methods - note importance of z_ for pipelines.config integration

---
+++ Controllers/Api/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ms8.Controllers.Api
+{
+    [RoutePrefix("ExampleSite/api/Sample")]
+    // this defines how to perform default route lookups on methods (e.g. this routes the action to a method of the same name like asp.net MVC, and sets Get() to the default action)
+    [Route("{action=Get}")]
+    public class TestController : ApiController
+    {
+        public IEnumerable<object> Get()
+        {
+            return new[]
+            {
+                new { foo="foo", bar="bar"},
+                new { foo="baz", bar="bonk" }
+            };
+        }
+
+        // GET ExampleSite/api/Sample/Foos
+        public string Foos()
+        {
+            return "hello world";
+        }
+    }
+}
--- code/Pipelines/Initialize/WebApiRouteInitializationProcessor.cs
+++ code/Pipelines/Initialize/WebApiRouteInitializationProcessor.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Dispatcher;
 using System.Web.Http.Routing;
 using Sitecore.Pipelines;
+using Sitecore.Services.Core;
 
 namespace ms8.code.Pipelines.Initialize
 {
@@ -8,8 +10,17 @@ namespace ms8.code.Pipelines.Initialize
     {
         public void Process(PipelineArgs args)
         {
-            GlobalConfiguration.Configuration.Routes.Add("api", new HttpRoute(
-                "api/{controller}/{action}"));
+            GlobalConfiguration.Configuration.Routes.Add("api", new HttpRoute("api/{controller}/{action}"));
+
+            GlobalConfiguration.Configure(config => config.MapHttpAttributeRoutes());
+
+            GlobalConfiguration.Configure(ReplaceControllerSelector);
+        }
+
+        private static void ReplaceControllerSelector(HttpConfiguration config)
+        {
+            config.Services.Replace(typeof (IHttpControllerSelector),
+                new CustomHttpControllerSelector(config, new NamespaceQualifiedUniqueNameGenerator()));
         }
     }
 }
